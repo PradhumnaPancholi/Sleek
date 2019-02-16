eb4e15ab28ff872ab9cad1bbd6a688ab99d57780
Pradhumna Panholi <user148657@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2019-02-16 02:43:41 -0500

    minor improv - added gestures for SideBar

--- Sleek/Controller/ChatVC.swift
+++ Sleek/Controller/ChatVC.swift
@@ -11,13 +11,18 @@ import UIKit
 class ChatVC: UIViewController {
     
     //outlets//
-    @IBOutlet weak var menu: UIButton!
+    @IBOutlet weak var menuBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        menu.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        // To make sidebar work with SWReveal and Views.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        //To add "drag" gesture for sidebar//
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        //To add "Tap" gesture to close SideBar//
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
     }
 
 }
