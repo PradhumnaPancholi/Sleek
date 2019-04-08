f3f2d603ef4378ccd919baa7bee9b5d2b9fedc0e
user146928 <pnpancholi6629.pnp@gmail.com>
2019-04-08 11:22:44 -0400

    added basic login functionality in LoginVC

--- Sleek/Controller/LoginVC.swift
+++ Sleek/Controller/LoginVC.swift
@@ -9,11 +9,16 @@
 import UIKit
 
 class LoginVC: UIViewController {
-
+    
+    //outlets//
+    
+    @IBOutlet weak var email: UITextField!    
+    @IBOutlet weak var password: UITextField!    
+    @IBOutlet weak var loader: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+        loader.isHidden = true
     }
-  
     //to close/dismiss Login VC//
     @IBAction func closeBtnPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -23,5 +28,27 @@ class LoginVC: UIViewController {
     //to load Create Account VC//
     @IBAction func createAccountPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_Create_Account, sender: nil)
+    }
+    //to login user//
+    @IBAction func loginBtnPressed(_ sender: Any) {
+        loader.isHidden = false
+        loader.startAnimating()
+        //to get input value and make sure it's not empty//
+        guard let userEmail = email.text, email.text != "" else {
+            return
+        }
+        guard let userPassword = password.text, password.text != "" else {
+            return
+        }
+        AuthService.instance.loginUser(email: userEmail, password: userPassword, completion: { (success) in
+            if success{
+                print("You are Logged In")
+                //for loader//
+                self.loader.stopAnimating()
+                self.loader.isHidden = true
+                //to dismiss segue//
+                self.dismiss(animated: true, completion: nil)
+            }
+        })
     }
 }
