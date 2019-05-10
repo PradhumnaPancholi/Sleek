b15110d0bebadf651ff20a6333283b2b3ce25530
user146928 <pnpancholi6629.pnp@gmail.com>
2019-05-09 22:58:11 -0400

    added channel modelclass

---
+++ Sleek/Model/Channel.swift
@@ -0,0 +1,15 @@
+//
+//  Channel.swift
+//  Sleek
+//
+//  Created by user146928 on 5/9/19.
+//  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
+//
+
+import Foundation
+
+class Channel {
+    public var id: String?
+    public var name: String?
+    public var desc: String?
+}
