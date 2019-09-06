8b3aedb6225fd702968de205a82c69162a21b679
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2019-09-06 14:32:21 -0400

    added Message model class

---
+++ Sleek/Model/Message.swift
@@ -0,0 +1,20 @@
+//
+//  Message.swift
+//  Sleek
+//
+//  Created by Pradhumna Pancholi on 2019-09-06.
+//  Copyright © 2019 Pradhumna Pancholi. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    public private (set) var id: String?
+    public private (set) var messageBody: String?
+    public private (set) var channelId: String?
+    public private (set) var userName: String?
+    public private (set) var userAvatarBG: String?
+    public private (set) var userAvatar: String?
+    public private (set) var timeStamp: String?
+}
